285c87c90caa04ebd0a16e33315a0621749be3a0
Nicolas Carlier <n.carlier@nunux.org>
2016-05-05 15:43:22 +0200

    chore(gulp): Upgrade dependencies and gulp configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -3,66 +3,63 @@ var gulp        = require('gulp'),
     ejs         = require('gulp-ejs'),
     yaml        = require('js-yaml'),
     markdown    = require('markdown'),
+    merge       = require('merge-stream'),
     browserSync = require('browser-sync'),
     deploy      = require('gulp-gh-pages'),
     del         = require('del'),
     fs          = require('fs');
 
-
 var reload = browserSync.reload;
 
-gulp.task('templating', function () {
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+gulp.task('templates', ['clean'], function () {
   var context = yaml.safeLoad(fs.readFileSync('data.yml', 'utf-8'));
   context.markdown = markdown.parse;
-  gulp.src('src/templates/index.html')
-  .pipe(ejs(context))
-  .pipe(gulp.dest('dist/'));
-  gulp.src('src/templates/README.md')
+  var readme = gulp.src('src/templates/README.md')
   .pipe(ejs(context, {ext: '.md'}))
   .pipe(gulp.dest('dist/'));
+  var index = gulp.src('src/templates/index.html')
+  .pipe(ejs(context))
+  .pipe(gulp.dest('dist/'));
+  return merge(readme, index);
 })
 
-gulp.task('assets', function() {
-  gulp.src('src/assets/**').pipe(gulp.dest('dist/assets'));
+gulp.task('assets', ['clean'], function() {
+  return gulp.src('src/assets/**').pipe(gulp.dest('dist/assets'));
 });
 
-gulp.task('styles', function() {
-  gulp.src('src/styles/index.less')
+gulp.task('styles', ['clean'], function() {
+  return gulp.src('src/styles/index.less')
     .pipe(less())
     .pipe(gulp.dest('dist/css'));
-});
-
-gulp.task('watch', function() {
-  // Watch template files
-  gulp.watch('src/templates/**/*', ['templating', reload]);
-  // Watch data file
-  gulp.watch('data.yml', ['templating', reload]);
-  // Watch style files
-  gulp.watch('src/styles/**/*.less', ['styles', reload]);
-  // Watch javascript files
-  gulp.watch('src/scripts/**/*.js', ['scripts', reload]);
-  // Watch image files
-  gulp.watch('src/images/**/*', ['images', reload]);
-});
-
-gulp.task('serve', ['default', 'watch'], function() {
-  browserSync({
-    server: {
-      baseDir: "./dist"
-    }
-  });
 });
 
 gulp.task('site', function () {
   return gulp.src('./dist/**/*').pipe(deploy({branch: 'master'}));
 });
 
-gulp.task('clean', function(cb) {
-  del(['dist'], cb)
+gulp.task('build', ['templates', 'styles', 'assets']);
+
+gulp.task('serve', ['build'], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+  // Watch template files
+  gulp.watch('src/templates/**/*', ['builds', reload]);
+  // Watch data file
+  gulp.watch('data.yml', ['build', reload]);
+  // Watch style files
+  gulp.watch('src/styles/**/*.less', ['build', reload]);
+  // Watch javascript files
+  gulp.watch('src/scripts/**/*.js', ['build', reload]);
+  // Watch image files
+  gulp.watch('src/images/**/*', ['build', reload]);
+
 });
 
-gulp.task('default', ['clean'], function() {
-  gulp.start('assets');
-  gulp.start('styles');
-  gulp.start('templating');
-});
+gulp.task('default', ['build']);
